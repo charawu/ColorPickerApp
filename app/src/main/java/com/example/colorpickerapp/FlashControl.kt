874c5c07d114c2815ca2577f54df3021702c66e5
harukimisora <liu.cors@qq.com>
2026-01-31 16:01:28 +0800

    手电筒基本功能完成

--- app/src/main/java/com/example/colorpickerapp/FlashControl.kt
+++ app/src/main/java/com/example/colorpickerapp/FlashControl.kt
@@ -1,12 +1,18 @@
 package com.example.colorpickerapp
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.VibrationEffect
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -14,15 +20,22 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import com.example.colorpickerapp.VibrationHelper.vibrateWithPredefinedEffect
 import com.google.android.material.slider.Slider
 
 class FlashControl : AppCompatActivity() {
 
+    private var TAG = "fuckFlashControl"
+    private lateinit var cameraManager: CameraManager
+    private var cameraID = ""
+    private var maxBrightness: Int = 1
+    private var defaultBrightness = 1
+    private var isFlashOn: Boolean = false
+
     companion object{
         private const val REQUEST_CAMERA_PERMISSION = 1001
     }
-    private lateinit var flashlightManger: AdvancedFlashlightManager
     private lateinit var flashButton: Button
     private lateinit var lightnessSlide: Slider
     private lateinit var brightnessTextView: TextView
@@ -49,72 +62,94 @@ class FlashControl : AppCompatActivity() {
     }
 
     private fun initFlashlightManager(){
-        flashlightManger = AdvancedFlashlightManager(this)
+        try {
+            cameraID = cameraManager.cameraIdList.firstOrNull() ?:return
 
-        if (!flashlightManger.isFlashlightAvailable()){
-            Toast.makeText(
-                this,
-                "设备不支持手电筒",
-                Toast.LENGTH_SHORT
-            ).show()
+            val characteristics = cameraManager.getCameraCharacteristics(cameraID)
 
-            flashButton.isEnabled = false
-            return
+            //检查获取到的相机是否有闪光灯
+            val hasFlash = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE) ?: false
+            if (!hasFlash){
+                flashButton.isEnabled = false
+                flashButton.setText("on flash!")
+                return
+            }
+
+            //获取最大亮度
+            maxBrightness = characteristics.get(
+                CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL) ?: 1
+
+            //获取默认亮度
+             defaultBrightness = characteristics.get(
+                 CameraCharacteristics.FLASH_INFO_STRENGTH_DEFAULT_LEVEL) ?: 1
+
+            lightnessSlide.valueTo = maxBrightness.toFloat()
+            lightnessSlide.valueFrom = 1.toFloat()
+
+
+            //设置brightnessTextView的值
+            var brightnessText = (defaultBrightness * 100 / maxBrightness).toString()
+            brightnessTextView.text = "$brightnessText%"
+            lightnessSlide.value = defaultBrightness.toFloat()
+        } catch (e: CameraAccessException) {
+            Log.d(TAG,"$e.message")
         }
 
-        //检查在Activity的打开活动
+        //检查在MainActivity的打开活动
         val autoTurnOn = intent.getBooleanExtra("auto_turn_on",false)
         if (autoTurnOn){
             Handler(Looper.getMainLooper()).postDelayed({
-                changeFlashlight()
+                updateBrightness(defaultBrightness)
             },300)
         }
     }
 
-    private fun updateUI(){
-
-    }
-
-    private fun updateBrightness(brightness: Int){
-        val success = flashlightManger.setBrightness(brightness)
-
-        if (success){
-            brightnessTextView.text = "${brightness}%"
-
-            if (flashlightManger.isFlashlightOn()){
-                flashlightManger.turnOnFlashlight(brightness)
-            }
-        }
+    private fun updateUI(value:Int){
+        var brightnessText  = (value * 100/ maxBrightness).toString()
+        brightnessTextView.text = "$brightnessText%"
     }
 
     private fun changeFlashlight(){
-        val success = flashlightManger.changeFlashlight()
 
-        if (success){
-            updateUI()
-
+        if (!isFlashOn){
+            updateBrightness(defaultBrightness)
+            flashButton.setText(R.string.buttonFlash_off)
         }else{
-            Toast.makeText(
-                this,
-                "手电筒打开失败",
-                Toast.LENGTH_SHORT
-            ).show()
+            turnOffFlash()
+            flashButton.setText(R.string.buttonFlash_on)
+        }
+
+        updateUI(defaultBrightness)
+        lightnessSlide.value = defaultBrightness.toFloat()
+    }
+
+    private fun updateBrightness(brightness: Int){
+        try {
+            if (brightness in 1..maxBrightness){
+                cameraManager.turnOnTorchWithStrengthLevel(cameraID,brightness)
+                isFlashOn = true
+            }
+        }catch (e: CameraAccessException){
+            Log.d(TAG,"${e.message}")
+        }
+    }
+
+    private fun turnOffFlash(){
+        try {
+            cameraManager.setTorchMode(cameraID,false)
+            isFlashOn = false
+        }catch (e: CameraAccessException){
+            Log.d(TAG,"${e.message}")
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (::flashlightManger.isInitialized){
-            flashlightManger.release()
+        try {
+            turnOffFlash()
+        } catch (e: CameraAccessException) {
+            Log.d(TAG,"${e.message}" + "手电筒关闭失败")
         }
-    }
-
-    private fun showToast(message: String){
-        Toast.makeText(
-            this,
-            message,
-            Toast.LENGTH_SHORT
-        ).show()
     }
 
     override fun onRequestPermissionsResult(
@@ -143,6 +178,9 @@ class FlashControl : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_control)
 
+        //初始化相机管理器
+        cameraManager = getSystemService(Context.CAMERA_SERVICE)as CameraManager
+
         initView()
         initFlashlightManager()
 
@@ -166,8 +204,21 @@ class FlashControl : AppCompatActivity() {
                 )
 
                 updateBrightness((value.toInt()))
-
+                updateUI(value.toInt())
             }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  //检查兼容新
+            val windowInsetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView())
+
+            // 判断是否为深色模式
+            val isDarkTheme = ((getResources().getConfiguration().uiMode and
+                    Configuration.UI_MODE_NIGHT_MASK)
+                    == Configuration.UI_MODE_NIGHT_YES)
+
+            // 设置状态栏图标颜色
+            windowInsetsController.setAppearanceLightStatusBars(!isDarkTheme)
         }
     }
 }
